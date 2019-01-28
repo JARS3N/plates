a219588fb0ff90a2b3ea36fe9bf26503ea0ebcd1
JARS3N <JARS3N@users.noreply.github.com>
2019-01-28 09:15:18 -0500

    Update plate8.R

--- R/plate8.R
+++ R/plate8.R
@@ -3,6 +3,7 @@ function() {
   data.frame(
     well_index = 1:8,
     Wells = paste0(LETTERS[1:8], "01"),
-    typical_background_wells_8 = c("Background", rep("Cells", 6), "Background")
+    typical_background_wells_8 = c("Background", rep("Cells", 6), "Background"),
+    stringsAsFactors=FALSE
   )
 }
