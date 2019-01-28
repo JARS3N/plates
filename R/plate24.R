eba30ef493fd329ac889e01cb29880fe30e92584
JARS3N <JARS3N@users.noreply.github.com>
2019-01-28 09:15:34 -0500

    Update plate24.R

--- R/plate24.R
+++ R/plate24.R
@@ -4,6 +4,7 @@ function() {
     well_index = 1:24,
     Wells = well_names_24(),
     loc = outer_wells_24(),
-    typical_background_wells = typical_background_wells_24()
+    typical_background_wells = typical_background_wells_24(),
+    stringsAsFactors=FALSE
   )
 }
