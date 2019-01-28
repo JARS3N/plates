83e3150f5128c6cb5c551cea91780eb5afdec0da
JARS3N <JARS3N@users.noreply.github.com>
2019-01-28 09:14:58 -0500

    Update plate96.R

--- R/plate96.R
+++ R/plate96.R
@@ -5,6 +5,7 @@ function() {
     Wells = well_names_96(),
     loc = outer_wells_96(),
     rings = rings_96(),
-    typical_background_wells = typical_background_wells_96()
+    typical_background_wells = typical_background_wells_96(),
+    stringsAsFactors=FALSE
   )
 }
