a5fe36e78ac94b0f6fc1947c791bf8782c69f41a
JARS3N <JARS3N@users.noreply.github.com>
2023-01-03 14:47:12 -0500

    Update plate8.R

--- R/plate8.R
+++ R/plate8.R
@@ -1,9 +1,1 @@
-plate8 <-
-function() {
-  data.frame(
-    well_index = 1:8,
-    Wells = paste0(LETTERS[1:8], "01"),
-    typical_background_wells_8 = c("Background", rep("Cells", 6), "Background"),
-    stringsAsFactors=FALSE
-  )
-}
+plate8 <- plate(8)
