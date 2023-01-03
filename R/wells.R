1cf51a156fa188b0a3e770f3be0551b688e1e973
JARS3N <JARS3N@users.noreply.github.com>
2023-01-03 14:56:59 -0500

    Update wells.R

--- R/wells.R
+++ R/wells.R
@@ -1,9 +1,9 @@
 wells <-
 function(n){
  x<-list(
-    "96"=well(96),
-    "24"=well(24),
-    "8"=well(8)
+    "96"=plates::well(96),
+    "24"=plates::well(24),
+    "8"=plates::well(8)
   )
  x[[as.character(n)]]()
 }
