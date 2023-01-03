ac7d60bd847b7df37c9e688c355125444057277d
JARS3N <JARS3N@users.noreply.github.com>
2023-01-03 14:59:50 -0500

    Update wells.R

--- R/wells.R
+++ R/wells.R
@@ -1,9 +1,9 @@
-wells <-
-function(n){
- x<-list(
-    "96"=plates::well(96),
-    "24"=plates::well(24),
-    "8"=plates::well(8)
-  )
- x[[as.character(n)]]()
-}
+#wells <-
+#function(n){
+# x<-list(
+#    "96"=plates::well(96),
+#    "24"=plates::well(24),
+#    "8"=plates::well(8)
+#  )
+# x[[as.character(n)]]()
+#}
