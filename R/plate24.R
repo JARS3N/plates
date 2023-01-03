e772ea6b54b26cd889d8131f108f2bd4c593a4b4
JARS3N <JARS3N@users.noreply.github.com>
2023-01-03 14:47:43 -0500

    Update plate24.R

--- R/plate24.R
+++ R/plate24.R
@@ -1,10 +1,1 @@
-plate24 <-
-function() {
-  data.frame(
-    well_index = 1:24,
-    Wells = well(24),
-    loc = outer_wells(24),
-    typical_background_wells = background(24),
-    stringsAsFactors=FALSE
-  )
-}
+plate24 <-plate(24)
